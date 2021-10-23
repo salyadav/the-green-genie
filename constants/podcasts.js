24ba3519fea753371f3df30db0c1f3d3ebeb1446
Tejas Bhuwania <35946746+Tejas2805@users.noreply.github.com>
2021-10-23 19:46:04 -0400

    Create podcasts.js

---
+++ constants/podcasts.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      podcastId: 1, 
+      link: "https://www.unpri.org/news-and-events/the-pri-podcast",
+      headText: "PRI podcasts",
+    }, {
+      podcastId: 2,
+      link: "https://www.spglobal.com/en/research-insights/podcasts/",
+      headText: "ESG Insider",
+    }, {
+      podcastId: 3,
+      link: "https://zerowastecountdown.com/",
+      headText: "The Zero Waste Countdown Podcast",
+    }, {
+      podcastId: 4,
+      link: "https://www.americaadapts.org/",
+      headText: "America Adapts - The Climate Change Podcast",
+    }, {
+      podcastId: 5,
+      link: "https://esgexperience.libsyn.com/",
+      headText: "The ESG Experiencee",
+    },
+  ];
