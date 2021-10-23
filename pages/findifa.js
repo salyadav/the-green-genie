86ec31f8f98ba397e707834cad1e38465f9a3148
Saloni Yadav <yadavsaloni2018@gmail.com>
2021-10-23 17:53:00 -0400

    dummy

--- pages/findifa.js
+++ pages/findifa.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import { useState } from 'react'
 
 
-export default function Estimate() {
+export default function FindIfa() {
   return (
     <div className={styles.container}>
         <div className={styles.card}>
