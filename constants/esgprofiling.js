14aa2155a8700cbf0e233277be2cf3cd373a50c8
Tejas Bhuwania <35946746+Tejas2805@users.noreply.github.com>
2021-10-23 18:20:09 -0400

    Update esgprofiling.js

--- constants/esgprofiling.js
+++ constants/esgprofiling.js
@@ -1,31 +1,43 @@
 export default [
     {
         qId: 1, 
-        question: "Please provide us a with a list of your current investments.",
+        question: "Please provide us a with a list of your current investments with their symbols separated by a comma. Example: AMZN, TSLA, NFLX",
         options: [],
     }, {
-        qId: 2, 
-        question: "How often do you use the public transport?"
+        qId: 2,
+        question: "Which State do you live in?",
+        options: [],
     }, {
         qId: 3, 
-        question: "How many miles do you travel via car?"
+        question: "How often do you use the public transport?",
+        options: ["Never", "Once a month", "Once a week", "Multiple times a week", "Everyday", "Always"],
     }, {
         qId: 4, 
-        question: "How much electricity do you use every month?"
+        question: "How many miles do you travel via personal car in a month?",
+        options: ["I do not have a car", "< 5,00", "5,01 - 1,000", "1,000 - 1,500", "> 1,500", "I only use my car"],
     }, {
         qId: 5, 
-        question: "How much gas do you use every month?"
+        question: "How much electricity (kWh) do you use every month?",
+        options: ["< 500", "500 - 799", "800 - 1,099", "1,100 - 1,399", "1,400 - 1,699", "> 1,700"],
     }, {
         qId: 6, 
-        question: "Do you have cryptocurrency? If yes, how many?"
+        question: "What is your gas bill on average every month?",
+        options: ["0", "1 - 25", "26 - 50", "51 - 75", "76 - 100", "101 - 125"],
     }, {
         qId: 7, 
-        question: "How much food do you consume over a month?"
+        question: "Do you have cryptocurrency? If yes, how many?",
+        options: ["I do not own cryptocurrency", "1-2", "3-5", "6-8", "> 8"],
     }, {
         qId: 8, 
-        question: "What is your dietary restriction?"
+        question: "How many cars do you own?",
+        options: ["None", "1", "2", "3", "4", "> 4"],
     }, {
         qId: 9, 
-        question: "Do you have an electric car?"
+        question: "What is your dietary restriction?",
+        options: ["Vegan", "Vegetarian", "Non-Vegetarian (no red meat)", "Non-Vegetarian (red meat)"],
+    }, {
+        qId: 10, 
+        question: "Do you have an electric car?",
+        options: ["I do not have a car", "I own an electric car", "I own a car but not electric"],
     },
 ];
