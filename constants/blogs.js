93ee83d2b7b63cacdab2a10512b0f674da8da9dc
Tejas Bhuwania <35946746+Tejas2805@users.noreply.github.com>
2021-10-23 19:38:01 -0400

    Create blogs.js

---
+++ constants/blogs.js
@@ -0,0 +1,23 @@
+export default [
+    {
+      blogId: 1, 
+      link: "https://www.greenstoneplus.com/blog/topic/esg",
+      headText: "Greenstone",
+    }, {
+      blogId: 2,
+      link: "https://www.msci.com/esg-blog-posts",
+      headText: "MSCI Blogs",
+    }, {
+      blogId: 3,
+      link: "https://blog.pimco.com/en/?tag=ESG",
+      headText: "PIMCO Blog",
+    }, {
+      blogId: 4,
+      link: "https://practicalesg.com/",
+      headText: "Practical ESG",
+    }, {
+      blogId: 5,
+      link: "https://blogs.cfainstitute.org/investor/tag/esg/",
+      headText: "CFA Institute",
+    },
+  ];
