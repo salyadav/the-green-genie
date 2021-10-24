543ee1174b9dd0665bdc8b3d38bf2b20cc2a3ce1
tejas bhuwania <tbhuwania3@gatech.edu>
2021-10-24 08:47:40 -0400

    update ifa page

--- constants/myifa.js
+++ constants/myifa.js
@@ -1,35 +1,39 @@
 export default [
-    {
-        ifaId: 1, 
-        name: "Andrew Smith",
-        contact: "+1 450-8152787",
-        email: "asmith@gmail.com",
-        url: "",
-        esgScore: "78",
-        noOfEsgClients: "12"
-    }, {
-        ifaId: 2, 
-        name: "Tehran Bhava",
-        contact: "+1 450-8152909",
-        email: "tehranb@gmail.com",
-        url: "",
-        esgScore: "64",
-        noOfEsgClients: "34"
-    }, {
-        ifaId: 3, 
-        name: "Ojehik Zandrew",
-        contact: "+1 450-8152321",
-        email: "ojzanrew@gmail.com",
-        url: "",
-        esgScore: "90",
-        noOfEsgClients: "11"
-    }, {
-        ifaId: 4, 
-        name: "Rajan Malhotra",
-        contact: "+1 450-8157873",
-        email: "rmalhotra@gmail.com",
-        url: "",
-        esgScore: "89",
-        noOfEsgClients: "8"
-    }
+  {
+    ifaId: 1,
+    name: "Tejas Bhuwania",
+    contact: "+1 450-8152787",
+    email: "tejas@gmail.com",
+    url: "",
+    esgScore: "78",
+    noOfEsgClients: "12",
+    link: "https://www.linkedin.com/in/tejas-bhuwania"
+  }, {
+    ifaId: 2,
+    name: "Tehran Bhava",
+    contact: "+1 450-8152909",
+    email: "tehranb@gmail.com",
+    url: "",
+    esgScore: "64",
+    noOfEsgClients: "34",
+    link: ""
+  }, {
+    ifaId: 3,
+    name: "Ojehik Zandrew",
+    contact: "+1 450-8152321",
+    email: "ojzanrew@gmail.com",
+    url: "",
+    esgScore: "90",
+    noOfEsgClients: "11",
+    link: ""
+  }, {
+    ifaId: 4,
+    name: "Rajan Malhotra",
+    contact: "+1 450-8157873",
+    email: "rmalhotra@gmail.com",
+    url: "",
+    esgScore: "89",
+    noOfEsgClients: "8",
+    link: ""
+  }
 ];
--- pages/findmyifa.js
+++ pages/findmyifa.js
@@ -7,7 +7,7 @@ const Card = ({data}) => {
     return (
         <div className={styles.card}>
             <Image src="/profilepic.png" width={96} height={96} />
-            <h1 className="font-bold text-green-600 text-lg">{data.name}</h1>
+          <h1><a href={data.link}><span className="font-bold text-green-600 text-lg">{data.name}</span></a></h1>
             <div className="text-sm text-gray-400 py-1">{data.email}</div>
             <div className="text-sm text-gray-500">{data.contact}</div>
             <div className="text-sm pt-4">
@@ -20,9 +20,14 @@ const Card = ({data}) => {
 
 export default function FindIfa() {
   return (
-      <main className={styles.container}>
+      <main>
         <div>
-            <h1 className="text-4xl font-bold my-10">Our top matched IFAs for you</h1>
+          <div className="bg-green-500 py-20">
+            <h1 className={styles.title}>
+              <span className="font-bold">Our top matched IFAs for you</span>
+            </h1>
+            <div className="flex justify-center text-gray-800 text-3xl pl-4">Find your Genie!</div>
+          </div>
             <div className="flex cursor-pointer">
                 {
                     ifaList.map((val, ind) => (
